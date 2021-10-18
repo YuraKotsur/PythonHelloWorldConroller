427fa848206a19a52a4fae1a2b094c8665d9db65
YuraKotsur <yurakotsur03@gmail.com>
2021-10-18 11:38:23 +0300

    string from url to db

--- HelloWorldController/HelloWorld/models.py
+++ HelloWorldController/HelloWorld/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class NameFromUrl(models.Model):
+	variable = models.CharField(max_length = 200)
+
+	def __str__(self):
+		return self.variable
+
+
+
+
 # Create your models here.
--- HelloWorldController/HelloWorld/views.py
+++ HelloWorldController/HelloWorld/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import NameFromUrl
 
 # Create your views here.
 
@@ -7,4 +8,6 @@ def index(request):
     return HttpResponse('Hello World')
 
 def return_var_from_url(request, name):
+    saver = NameFromUrl(variable=name)
+    saver.save()
     return HttpResponse(name)
